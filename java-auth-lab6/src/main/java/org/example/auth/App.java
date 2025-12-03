992495bef70ffad01e2fc220185738629b652fc7
GwynnBleidd61 <albakov.adam2015@gmail.com>
2025-12-03 16:12:36 +0300

    Init Maven project for auth lab, add basic structure

---
+++ java-auth-lab6/src/main/java/org/example/auth/App.java
@@ -0,0 +1,7 @@
+package org.example.auth;
+
+public class App {
+    static void main() {
+        System.out.println("System Auth System starting...");
+    }
+}
---
+++ java-auth-lab6/src/main/java/org/example/auth/User.java
@@ -0,0 +1,8 @@
+package org.example.auth;
+
+public class User {
+    private long id;
+    private String username;
+    private String passwordHash;
+    private String role;
+}
